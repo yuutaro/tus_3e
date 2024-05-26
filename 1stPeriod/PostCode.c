88f907f7559d8af55d2dd8f5458b922353fdf446
yuutaro-Laptop <yuutaro.mikasa@gmail.com>
2024-05-26 11:33:27 +0900

    first commit

---
+++ 1stPeriod/PostCode.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HASHSIZE 100
+#define SEARCHED_TEXT1 "input1000.txt"
+#define SEARCHED_TEXT2 "input10000.txt"
+#define SEARCHED_TEXT3 "input100000.txt"
+
+struct cell {
+  char *p_code; // 郵便番号
+  char *address; // 住所
+  struct cell *p_next; // 次の郵便番号のセルへのポインタ
+  struct cell *a_next; // 次の住所のセルへのポインタ
+};
+
+// key: 郵便番号, value: 住所のハッシュテーブル
+struct cell *p_table[HASHSIZE];
+
+// key: 住所, value: 郵便番号のハッシュテーブル
+struct cell *a_table[HASHSIZE];
+
+// ハッシュ関数（utf-8）
+unsigned int hash(const char *str) {
+  unsigned int hashval = 0;
+  unsigned char *p = (unsigned char *)str;
+
+  while (*p != '\0') {
+    hashval = *p + 31 * hashval;
+    p++;
+  }
+  unsigned int result = hashval % HASHSIZE;
+  // printf("%s: %d\n", str, result);
+  return result;
+}
+
+// ハッシュテーブルにデータを追加する関数
+struct cell *new_cell(char *p_code, char *address){
+  struct cell *p = (struct cell *)malloc(sizeof(struct cell));
+  if(p == NULL){
+    perror("Error: malloc\n");
+    exit(1);
+  }
+  p->p_code = p_code;
+  p->address = address;
+  p->p_next = NULL;
+  p->a_next = NULL;
+  return p;
+}
+
+// ハッシュテーブルの情報を表示
+void hash_info(){
+  int sum_p = 0, sum_a = 0;
+  int i, length;
+  struct cell *p;
+
+  for(i=0; i<HASHSIZE; i++){
+    length = 0;
+    p = p_table[i];
+    while(p != NULL){
+      length++;
+      p = p->p_next;
+    }
+    // printf("p_codetable[%d]: %d\n", i, length);
+    sum_p += length;
+  }
+  printf("p_codetable: %d\n", sum_p);
+
+  for(i=0; i<HASHSIZE; i++){
+    length = 0;
+    p = a_table[i];
+    while(p != NULL){
+      length++;
+      p = p->a_next;
+    }
+    // printf("a_codetable[%d]: %d\n", i, length);
+    sum_a += length;
+  }
+  printf("a_codetable: %d\n", sum_a);
+  
+}
+
+// 郵便番号をテーブルから検索し、見つかった場合はそのセルへのポインタを返す
+struct cell *search_p_code(char *p_code){
+  int index = hash(p_code);
+  struct cell *p = p_table[index];
+  while(p != NULL){
+    if(strcmp(p->p_code, p_code) == 0){
+      return p;
+    }
+    p = p->p_next;
+  }
+  return NULL;
+}
+
+// 住所をテーブルから検索し、見つかった場合はそのセルへのポインタを返す
+struct cell *search_address(char *address){
+  int index = hash(address);
+  struct cell *p = a_table[index];
+  while(p != NULL){
+    if(strcmp(p->address, address) == 0){
+      return p;
+    }
+    p = p->a_next;
+  }
+  return NULL;
+}
+
+int main(void){
+  // 郵便番号-住所対応表を読み込み
+  FILE *fp;
+  char PostCodeBuffer[8];
+  char AddressBuffer[1024];
+
+  // ファイル読み込み
+  fp = fopen("postal.txt", "r"); // ファイル読み込みモード(r)でファイルを開く
+  if(fp == NULL){
+    perror("Error: postal.txtファイルがありません\n");
+    return 1;
+  }
+  while (fscanf(fp, "%s %s", PostCodeBuffer, AddressBuffer) != EOF) {
+    // printf("%s %s\n", PostCodeBuffer, AddressBuffer);
+
+    // ハッシュテーブルにデータを追加
+    struct cell *p = new_cell(PostCodeBuffer, AddressBuffer);
+    int p_index = hash(PostCodeBuffer);
+    int a_index = hash(AddressBuffer);
+
+    // 郵便番号-住所対応表
+    if(p_table[p_index] == NULL){
+      p_table[p_index] = p;
+    }else{
+      p->p_next = p_table[p_index];
+      p_table[p_index] = p;
+    }
+
+    // 住所-郵便番号対応表
+    if(a_table[a_index] == NULL){
+      a_table[a_index] = p;
+    }else{
+      p->a_next = a_table[a_index];
+      a_table[a_index] = p;
+    }
+
+  }
+  fclose(fp);
+
+  // hash_info();
+
+  struct cell *p = p_table[0];
+  printf("%s %s\n", p->p_code, p->p_next->p_code);
+
+  return 0;
+}
+
