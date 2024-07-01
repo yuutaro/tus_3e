64da0bb853271363fd6cedca4cc9218074d79fb2
yutaro-desktop <yuutaro.mikasa@gmail.com>
2024-07-02 01:08:33 +0900

    ランダムAIができたのでとりあえずコミット

---
+++ 3edPeriod/AIClient/AIClient.java
@@ -0,0 +1,225 @@
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class AIClient {
+  final static int BLACK = 1;
+  final static int WHITE = -1;
+
+  private Socket socket;
+  private PrintWriter out;
+  private BufferedReader in;
+  private int myColor;
+  private int currentTurn;
+  private int[][] board = new int[8][8];
+
+  Random random = new Random();
+
+  public AIClient(String serverAddress, int serverPort) {
+    try {
+      socket = new Socket(serverAddress, serverPort);
+      out = new PrintWriter(socket.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    } catch (Exception e) {
+      System.out.println("サーバーに接続できませんでした。");
+      System.exit(1);
+    }
+
+    // 受信部分追加
+    new Thread(new Runnable() {
+      public void run() {
+        try {
+          String line;
+          while ((line = in.readLine()) != null) {
+            handleServerMessage(line);
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }).start();
+  }
+
+  private void handleServerMessage(String message) {
+    String command = message.split(" ")[0];
+    
+    switch (command) {
+      case "START":
+        String[] startTokens = message.split(" ");
+        myColor = Integer.parseInt(startTokens[1]);
+        System.out.println("自分の色: " + (myColor == BLACK ? "BLACK" : "WHITE") + "\n");
+        break;
+        
+      case "BOARD":
+        String[] boardTokens = message.split(" ");
+        for (int i = 1; i < boardTokens.length; i++) {
+          board[(i - 1) / 8][(i - 1) % 8] = Byte.parseByte(boardTokens[i]);
+        }
+        System.out.println("現在の盤面\n");
+        for (int i = 0; i < 8; i++) {
+          for (int j = 0; j < 8; j++) {
+            if (board[i][j] == BLACK) {
+              System.out.print("○ ");
+            } else if (board[i][j] == WHITE) {
+              System.out.print("● ");
+            } else {
+              System.out.print("- ");
+            }
+          }
+          System.out.print("\n");
+        }
+        System.out.println();
+        break;
+        
+      case "TURN":
+        int turn = Integer.parseInt(message.split(" ")[1]);
+        currentTurn = turn;
+        System.out.println("現在のターン: " + (currentTurn == BLACK ? "BLACK" : "WHITE") + "\n");
+
+        // 自動で石を置く処理を仮置き
+        if (currentTurn == myColor) {
+          int[] pos = randomPut(board, myColor);
+          out.println("PUT " + pos[0] + " " + pos[1]);
+        }
+
+        break;
+        
+      case "SAY":
+        System.out.println("メッセージ" + message.substring(4) + "\n");
+        break;
+        
+      case "ERROR":
+        String errorCode = message.split(" ")[1];
+        switch (errorCode) {
+          case "1":
+            System.out.println("書式が間違っています。\n");
+            break;
+          case "2":
+            System.out.println("選択した番目には自身の色の石は置けません。\n");
+            break;
+          case "3":
+            System.out.println("相手のターンです。\n");
+            break;
+          case "4":
+            System.out.println("処理できない命令です。\n");
+            break;
+          default:
+            System.out.println("未知のエラーが発生しました。\n");
+            break;
+        }
+        break;
+        
+      case "NICK":
+        System.out.println("ニックネームが設定されました。\n");
+        break;
+        
+      case "END":
+        System.out.println("ゲーム終了: " + message.substring(4) + "\n");
+        break;
+        
+      case "CLOSE":
+        System.out.println("対戦相手とサーバーとの接続が切断されました。\n");
+        break;
+        
+      default:
+        System.out.println("不明なメッセージが送られてきました。\n");
+        System.out.println(message + "\n");
+        break;
+    }
+  }
+
+// 石が置けるかどうかを判定する関数
+private boolean isValidMove(int[][] board, int x, int y, int color) {
+  // すでに石が置かれている場所には置けない
+  if (board[x][y] != 0) {
+    return false;
+  }
+
+  int opponent = -color;
+
+  // 8方向をチェック
+  int[][] directions = {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
+  
+  for (int[] dir : directions) {
+    int dx = dir[0];
+    int dy = dir[1];
+    int nx = x + dx;
+    int ny = y + dy;
+    
+    // 隣が相手の石かチェック
+    if (nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && board[nx][ny] == opponent) {
+      nx += dx;
+      ny += dy;
+      // その方向にさらに進む
+      while (nx >= 0 && nx < 8 && ny >= 0 && ny < 8) {
+        if (board[nx][ny] == color) {
+          // 自分の石で挟めていればtrue
+          return true;
+        } else if (board[nx][ny] == 0) {
+          // 空白マスに到達したらこの方向は無効
+          break;
+        }
+        nx += dx;
+        ny += dy;
+      }
+    }
+  }
+  
+  // どの方向でも挟めなかった
+  return false;
+}
+
+// とりあえずランダムに石を置く関数
+private int[] randomPut(int[][] board, int color) {
+  ArrayList<int[]> validMoves = new ArrayList<>();
+  int[][] debugBoard = new int[8][8];
+  
+  // 全マスをチェックして有効な手を列挙
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (isValidMove(board, i, j, color)) {
+        validMoves.add(new int[]{i, j});
+        debugBoard[i][j] = 1; // デバッグ用に置ける位置を1で表示
+      }
+    }
+  }
+  
+  // デバッグ用の表示
+  System.out.println("置ける位置:");
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      System.out.print(debugBoard[i][j] + " ");
+    }
+    System.out.println();
+  }
+  System.out.println();
+  
+  // 有効な手がない場合
+  if (validMoves.isEmpty()) {
+    System.out.println("置ける位置がありません。");
+    return null;
+  }
+  
+  // ランダムに選択
+  Random random = new Random();
+  int randomIndex = random.nextInt(validMoves.size());
+  int[] selectedMove = validMoves.get(randomIndex);
+  
+  System.out.println("選択された位置: (" + selectedMove[0] + ", " + selectedMove[1] + ")");
+  
+  return selectedMove;
+}
+
+  public static void main(String args[]) {
+    if (args.length != 2) {
+      System.out.println("Usage: java AIClient <server address> <server port>");
+      System.exit(1);
+    }
+    new AIClient(args[0], Integer.parseInt(args[1]));
+  }
+}
--- 3edPeriod/Client2/OthelloClient2.java
+++ 3edPeriod/Client2/OthelloClient2.java
@@ -1,10 +1,10 @@
-package kadai.Client1;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.io.*;
+import java.net.*;
 
-class OthelloClient extends JFrame {
+class OthelloClient2 extends JFrame {
   final static int BLACK = 1;
   final static int WHITE = -1;
 
@@ -12,29 +12,48 @@ class OthelloClient extends JFrame {
   private JTextArea ta;
   private JLabel label;
   private OthelloCanvas canvas;
+  private Socket socket;
+  private PrintWriter out;
+  private BufferedReader in;
 
-  public OthelloClient() {
+  public OthelloClient2(String serverAddress, int serverPort) {
+    try {
+      socket = new Socket(serverAddress, serverPort);
+      out = new PrintWriter(socket.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
     this.setSize(640, 320);
     this.addWindowListener(new WindowAdapter() {
       public void windowClosing(WindowEvent e) {
         /* ウインドウが閉じられた時の処理 */
+        out.println("CLOSE");
         System.exit(0);
       }
     });
+
     tf = new JTextField(40);
     tf.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         /* テキストフィールドに文字が入力された時の処理 */
-        if (tf.getText().equals("quit")) {
+        String message = tf.getText();
+        if (message.equals("quit")) {
+          out.println("CLOSE");
           System.exit(0);
+        } else if (message.startsWith("NICK ")) {
+          out.println(message);
+        } else if (message.startsWith("SAY ")) {
+          out.println(message);
+        } else {
+          ta.append("Unknown command.\n");
         }
-
-        // ここに送信部分追加
-
-        System.out.println(tf.getText());
         tf.setText(""); // テキストフィールドの文字を初期化
       }
     });
+
     ta = new JTextArea(18, 40);
     ta.setLineWrap(true);
     ta.setEditable(false);
@@ -43,7 +62,7 @@ class OthelloClient extends JFrame {
     JPanel mainp = (JPanel) getContentPane();
     JPanel ep = new JPanel();
     JPanel wp = new JPanel();
-    canvas = new OthelloCanvas();
+    canvas = new OthelloCanvas(out);
     GridLayout gl = new GridLayout(1, 2);
     gl.setHgap(5);
     mainp.setLayout(gl);
@@ -58,11 +77,44 @@ class OthelloClient extends JFrame {
     this.setVisible(true);
 
     // 受信部分追加
+    new Thread(new Runnable() {
+      public void run() {
+        try {
+          String line;
+          while ((line = in.readLine()) != null) {
+            handleServerMessage(line);
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }).start();
+  }
 
+  private void handleServerMessage(String message) {
+    if (message.startsWith("BOARD ")) {
+      String[] tokens = message.split(" ");
+      byte[][] board = new byte[8][8];
+      for (int i = 1; i < tokens.length; i++) {
+        board[(i - 1) / 8][(i - 1) % 8] = Byte.parseByte(tokens[i]);
+      }
+      canvas.updateBoard(board);
+    } else if (message.startsWith("TURN ")) {
+      int turn = Integer.parseInt(message.split(" ")[1]);
+      label.setText("Turn: " + (turn == BLACK ? "BLACK" : "WHITE"));
+    } else if (message.startsWith("SAY ")) {
+      ta.append(message.substring(4) + "\n");
+    } else if (message.startsWith("END ")) {
+      ta.append("Game Over: " + message.substring(4) + "\n");
+    }
   }
 
   public static void main(String args[]) {
-    new OthelloClient();
+    if (args.length != 2) {
+      System.out.println("Usage: java OthelloClient <server address> <server port>");
+      System.exit(1);
+    }
+    new OthelloClient2(args[0], Integer.parseInt(args[1]));
   }
 }
 
@@ -80,20 +132,30 @@ class OthelloCanvas extends JPanel {
       { 0, 0, 0, 0, 0, 0, 0, 0 },
       { 0, 0, 0, 0, 0, 0, 0, 0 }
   }; // サンプルデータ
+  private PrintWriter out;
 
-  public OthelloCanvas() {
+  public OthelloCanvas(PrintWriter out) {
+    this.out = out;
     this.addMouseListener(new MouseAdapter() {
       public void mousePressed(MouseEvent e) {
         /* 盤目上でマウスがクリックされた時の処理 */
         Point p = e.getPoint();
-        System.out.println("" + p); // デバッグ用表示
-
-        // ここに送信部分追加
+        int x = (p.x - startx) / gap;
+        int y = (p.y - starty) / gap;
+        if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+          out.println("PUT " + x + " " + y);
+        }
       }
     });
   }
 
+  public void updateBoard(byte[][] board) {
+    this.board = board;
+    repaint();
+  }
+
   public void paintComponent(Graphics g) {
+    super.paintComponent(g);
     g.setColor(new Color(0, 180, 0));
     g.fillRect(startx, starty, gap * 8, gap * 8);
 
@@ -104,10 +166,10 @@ class OthelloCanvas extends JPanel {
     }
     for (int i = 0; i < 8; i++) {
       for (int j = 0; j < 8; j++) {
-        if (board[i][j] == OthelloClient.BLACK) {
+        if (board[i][j] == OthelloClient2.BLACK) {
           g.setColor(Color.BLACK);
           g.fillOval(startx + gap * i, starty + gap * j, gap, gap);
-        } else if (board[i][j] == OthelloClient.WHITE) {
+        } else if (board[i][j] == OthelloClient2.WHITE) {
           g.setColor(Color.WHITE);
           g.fillOval(startx + gap * i, starty + gap * j, gap, gap);
         }
